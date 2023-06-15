b320c2b3f0c45294ba4795273cb9c432f3e01115
Awet Mekonnen <awet806645@gmail.com>
2023-06-15 15:43:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - exit point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building amultilingual puzzle");
+	return (0);
+}
